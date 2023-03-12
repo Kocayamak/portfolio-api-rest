f5df736b28701b52261494b3a16844b4a25cd9c5
Kocayamak <mikailkocayamak@gmail.com>
2023-03-12 21:51:39 +0300

    mongo dbb connect

---
+++ config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    mongoose.connect(process.env.MONGO_DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB Connected...");
+    })
+    .catch((error) => {
+        console.error(`Error: ${error.message}`);
+    });
+};
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
+import connectDB from "./config/database.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const port = process.env.PORT || 5000;
 
@@ -13,6 +16,16 @@ app.get("/", (req, res) => {
   res.json({ msg: "Welcome to the ContactKeeper API..." });
 });
 
-app.listen(port, () =>
-  console.log(`Server started on port http://localhost:${port}`)
-);
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(port, () =>
+      console.log(`Server started on port http://localhost:${port}`)
+    );
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
